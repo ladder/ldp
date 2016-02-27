0909508f4afe8ae1d3a46ccaa187abe1b93c661d
MJ Suhonos <mj@suhonos.ca>
2016-02-27 11:52:21 -0500

    Stub out initial files

---
+++ app/ladder.rb
@@ -0,0 +1,56 @@
+require 'rack/ldp'
+require 'sinatra/base'
+
+class RDF::Ladder < Sinatra::Base
+
+  use Rack::LDP::ContentNegotiation
+  use Rack::LDP::Errors
+  use Rack::LDP::Responses
+  use Rack::ConditionalGet
+  use Rack::LDP::Requests
+
+  # Set defaults in case user has not configured values
+  configure do
+    # TODO: we're not actually going to use a repository
+    # will have to modify RDFSource (or Resource)
+    set :repository, RDF::Repository.new
+  end
+
+  get '/*' do
+    RDF::LDP::Container.new(RDF::URI(request.url), settings.repository)
+      .create('', 'text/turtle') if settings.repository.empty?
+    RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+  end
+
+  patch '/*' do
+    RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+  end
+
+  post '/*' do
+    RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+  end
+
+  put '/*' do
+    begin
+      RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+    rescue RDF::LDP::NotFound
+      model = request.env.fetch('HTTP_LINK', '')
+      RDF::LDP::Resource.interaction_model(model)
+        .new(RDF::URI(request.url), settings.repository)
+    end
+  end
+
+  options '/*' do
+    RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+  end
+
+  head '/*' do
+    RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+  end
+
+  delete '/*' do
+    RDF::LDP::Resource.find(RDF::URI(request.url), settings.repository)
+  end
+
+  run! if app_file == $0
+end
---
+++ ladder-ldp.gemspec
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby -rubygems
+# -*- encoding: utf-8 -*-
+
+Gem::Specification.new do |gem|
+  gem.version            = File.read('VERSION').chomp
+  gem.date               = File.mtime('VERSION').strftime('%Y-%m-%d')
+
+  gem.name               = 'ladder-ldp'
+  gem.homepage           = 'https://github.com/ladder/ladder'
+  gem.license            = 'Apache-2.0'
+  gem.summary            = 'An opinionated LDP server based on RDF::LDP.'
+  gem.description        = 'TODO'
+
+  gem.authors            = ['MJ Suhonos']
+  gem.email              = 'mj@suhonos.ca'
+
+  gem.platform           = Gem::Platform::RUBY
+  gem.files              = %w(AUTHORS README.md VERSION) +
+                           Dir.glob('lib/**/*.rb') + Dir.glob('app/**/*.rb')
+  gem.bindir             = %q(bin)
+  gem.executables        = %w(ladder)
+  gem.default_executable = gem.executables.first
+  gem.require_paths      = %w(lib app)
+  gem.has_rdoc           = false
+
+  gem.required_ruby_version      = '>= 2.0.0'
+  gem.requirements               = []
+
+  gem.add_runtime_dependency      'rdf-ldp'
+  gem.add_runtime_dependency      'mongoid-grid_fs'
+  gem.add_runtime_dependency      'active_triples-mongoid_strategy'
+
+  gem.add_development_dependency 'pry'
+  gem.add_development_dependency 'rspec'
+  gem.add_development_dependency 'yard'
+
+  gem.post_install_message       = nil
+end
